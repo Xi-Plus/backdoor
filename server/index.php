e9aea00ed77f9a1a99bb3ba148d3baf003382e83
Xi-Plus <huangxuanyuxiplus@gmail.com>
2015-09-22 21:17:26 +0800

    Add result file

--- server/index.php
+++ server/index.php
@@ -9,7 +9,11 @@ while($token_run==$token_file){
 	$text=file_get_contents($config["url"]."code.php?pwd=".$config["password"]);
 	echo "Run ".date("H:i:s")."\r\n";
 	echo $text."\r\n";
+	ob_start();
 	eval($text);
+	$result=ob_get_contents();
+	ob_end_clean();
+	file_put_contents("result.txt",@file_get_contents("result.txt").$result);
 	sleep($config["sleeptime"]);
 	$token_file=@file_get_contents("token.txt");
 }
